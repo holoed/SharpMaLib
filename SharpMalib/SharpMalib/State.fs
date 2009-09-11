23501c75582a41708b564aab02f5e3a0eaee90c1
holoed_cp <SND\holoed_cp>
2009-09-11 23:31:21 +0000

    First check in - Identity Monad - Maybe Monad - State Monad
    Added some tests for identity monad.
    Added attempt to check the Monad Laws using QuickCheck.

---
+++ SharpMalib/SharpMalib/State.fs
@@ -0,0 +1,27 @@
+﻿
+// * **********************************************************************************************
+// * Copyright (c) Edmondo Pentangelo. 
+// *
+// * This source code is subject to terms and conditions of the Microsoft Public License. 
+// * A copy of the license can be found in the License.html file at the root of this distribution. 
+// * By using this source code in any fashion, you are agreeing to be bound by the terms of the 
+// * Microsoft Public License.
+// *
+// * You must not remove this notice, or any other, from this software.
+// * **********************************************************************************************
+
+namespace SharpMalib
+module StateMonad
+
+// State Monad
+
+type State<'state,'a> = State of ('state -> 'a * 'state)
+
+type StateBuilder() =
+    member this.Return a = State(fun s -> a, s)
+    member this.Bind (State x, f) = State(fun s -> let (y, s') = x s in (f y) s') 
+
+let get = State(fun s -> (s,s))
+let put s = State(fun _ -> ((), s))
+    
+let state = StateBuilder()                                
