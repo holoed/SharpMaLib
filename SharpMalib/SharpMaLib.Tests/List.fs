2e01344a8116191ac75719a5a4c9ce63ac517ee9
holoed_cp <SND\holoed_cp>
2009-09-17 21:19:30 +0000

    Added List Monad purely for educational purposes since is already present in both C# (LINQ) and F# (List Comprehensions)
    Added tests for List Monad

---
+++ SharpMalib/SharpMaLib.Tests/List.fs
@@ -0,0 +1,46 @@
+﻿// * **********************************************************************************************
+// * Copyright (c) Edmondo Pentangelo. 
+// *
+// * This source code is subject to terms and conditions of the Microsoft Public License. 
+// * A copy of the license can be found in the License.html file at the root of this distribution. 
+// * By using this source code in any fashion, you are agreeing to be bound by the terms of the 
+// * Microsoft Public License.
+// *
+// * You must not remove this notice, or any other, from this software.
+// * **********************************************************************************************
+
+open NUnit.Framework
+open FsCheck
+open NUnitFsCheck
+open SharpMalib.ListMonad
+
+let (>>=) m f = list.Bind (m, f)
+let unit = list.Return
+let id = fun x -> x
+
+[<TestFixture>]
+type ListTests =
+    new() = {}
+    
+    [<Test>]
+    member x.MonadLaws() =
+        // Left unit  
+        quickCheck (fun m f a -> ((unit a) >>= f) = f a) 
+        // Right unit
+        quickCheck (fun m  -> (m >>= unit) = m)
+        // Associative
+        quickCheck (fun m f g-> ((m >>= f) >>= g) = (m >>= (fun x -> f x >>= g)))
+        
+    [<Test>]
+    member x.MonadLawsInTermsOfMapAndJoin() =
+        let f x = x / 2
+        let g x = x - 2
+        quickCheck (fun x -> map id x = id x)
+        quickCheck (fun x -> map (f << g) x = ((map f) << (map g)) x)
+        quickCheck (fun x -> (map f << unit) x = (unit << f) x)
+        quickCheck (fun x -> (map f << join) x = (join << (map (map f))) x)
+        quickCheck (fun x -> (join << unit) x = id x)
+        quickCheck (fun x -> (join << map unit) x = id x)
+        quickCheck (fun x -> (join << map join) x = (join << join) x)
+        quickCheck (fun m k -> m >>= k = join(map k m))
+        
---
+++ SharpMalib/SharpMalib/List.fs
@@ -0,0 +1,41 @@
+﻿// * **********************************************************************************************
+// * Copyright (c) Edmondo Pentangelo. 
+// *
+// * This source code is subject to terms and conditions of the Microsoft Public License. 
+// * A copy of the license can be found in the License.html file at the root of this distribution. 
+// * By using this source code in any fashion, you are agreeing to be bound by the terms of the 
+// * Microsoft Public License.
+// *
+// * You must not remove this notice, or any other, from this software.
+// * **********************************************************************************************
+
+namespace SharpMalib
+module ListMonad
+
+// List Monad
+// The List monad embodies the strategy of combining a chain of non-deterministic computations 
+// by applying the operations to all possible values at each step. It is useful when computations must deal with ambiguity. 
+// In that case it allows all possibilities to be explored until the ambiguity is resolved.
+
+type 'a List = Nil | Cons of 'a * 'a List
+
+let rec map f xs = match xs with
+                   | Nil -> Nil
+                   | Cons(x, xs') -> Cons(f(x), (map f xs'))
+
+let rec append xs ys = match (xs, ys) with
+                       | (Nil,_) -> ys
+                       | (Cons(x,xs'), _) -> Cons(x,append xs' ys)
+                
+let rec join xs = match xs with
+                  | Nil -> Nil
+                  | Cons(x, xs') -> append x (join xs')
+
+type ListBuilder() =
+    member this.Return a = Cons(a, Nil)
+    member this.Bind (m, f) = join (map f m) 
+    
+let list = ListBuilder()    
+
+      
+
