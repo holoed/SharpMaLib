01bdd49b72075f734047c216180d91f5310ab25d
holoed_cp <SND\holoed_cp>
2010-01-06 01:27:41 +0000

    Refactorings... Initial check in for Parser Monad + Tests

---
+++ SharpMalib/SharpMaLib.Tests/Parser.fs
@@ -0,0 +1,61 @@
+﻿// * **********************************************************************************************
+// * Copyright (c) Edmondo Pentangelo. 
+// *
+// * This source code is subject to terms and conditions of the Microsoft Public License. 
+// * A copy of the license can be found in the License.html file at the root of this distribution. 
+// * By using this source code in any fashion, you are agreeing to be bound by the terms of the 
+// * Microsoft Public License.
+// *
+// * You must not remove this notice, or any other, from this software.
+// * **********************************************************************************************
+
+namespace SharpMalib.Tests
+module ParserTests =
+    open NUnit.Framework
+    open FsCheck
+    open SharpMalib.Parser.ParserMonad
+
+    let (>>=) m f = parser.Bind (m, f)
+    let unit = parser.Return
+
+    let f x = Parser(fun s -> Some (x, s))
+
+    type Helper =
+     static member Some (a, s:string) = Some (a, Seq.toList s)
+     static member Some (x:string, y:string) = Some (Seq.toList x, Seq.toList y)
+
+    [<TestFixture>]
+    type ParserTests =
+        new() = {}
+        
+        [<Test>]
+        member this.Item() =
+            Assert.AreEqual (Helper.Some ('H', "ello"), parse item "Hello")
+            Assert.AreEqual (None, parse item "")
+
+        [<Test>]
+        member this.Ret() = 
+            Assert.AreEqual (Helper.Some ('X', "42"), parse (ret 'X') "42")
+
+        [<Test>]
+        member this.Fail() =
+            Assert.AreEqual (None , parse fail "Hello")
+
+        [<Test>]
+        member this.Or() =
+            let parserA = ret 'X'
+            let parserB = fail
+            let input = "42"
+            let assertValue = fun actual -> Assert.AreEqual (Helper.Some ('X', input), actual)
+            assertValue (parse (parserA +++ parserB) input)
+            assertValue (parse (parserB +++ parserA) input)
+
+        [<Test>]
+        member this.Sat() =
+            Assert.AreEqual (None, parse (sat 'X') "Hello")
+            Assert.AreEqual (Helper.Some ('H', "ello"), parse (sat 'H') "Hello")
+
+        [<Test>]
+        member this.StringP() =
+            Assert.AreEqual(None, parse (stringp (Seq.toList "Hello")) "World")
+            Assert.AreEqual(Helper.Some ("Hello", " World"), parse (stringp (Seq.toList "Hello")) "Hello World")
---
+++ SharpMalib/SharpMalib/Parser.fs
@@ -0,0 +1,54 @@
+﻿// * **********************************************************************************************
+// * Copyright (c) Edmondo Pentangelo. 
+// *
+// * This source code is subject to terms and conditions of the Microsoft Public License. 
+// * A copy of the license can be found in the License.html file at the root of this distribution. 
+// * By using this source code in any fashion, you are agreeing to be bound by the terms of the 
+// * Microsoft Public License.
+// *
+// * You must not remove this notice, or any other, from this software.
+// * **********************************************************************************************
+
+namespace SharpMalib.Parser
+module ParserMonad = 
+
+    type Parser<'a, 'b> = Parser of ('a list -> ('b * ('a list)) option)
+
+    let item = Parser (fun s -> match s with
+                                | x::xs -> Some (x, xs)
+                                | [] -> None)
+    
+    let ret x = Parser (fun s -> Some (x, s))
+
+    let fail = Parser (fun _ -> None)
+
+    let parse (Parser p) s = p (Seq.toList s)
+
+    let (+++) p q = Parser (fun s -> match parse p s with
+                                     | None-> parse q s
+                                     | result -> result)
+
+    type ParserMonad() =
+        // m a -> (a -> m b) -> m b
+        member this.Bind (m, f) = Parser (fun s -> match parse m s with
+                                                   | None -> None
+                                                   | Some (x, xs) -> parse (f x) xs)
+        // a -> m a
+        member thid.Return x = ret x
+
+        // m a -> m a
+        member this.ReturnFrom (x:Parser<'a, 'b>) = x
+
+    let parser = ParserMonad()
+
+    let sat ch = parser { let! x = item
+                          if ch = x then
+                             return x
+                          else
+                             return! fail }
+
+    let rec stringp s = match s with
+                        | x::xs -> parser { let! y = sat x
+                                            let! ys = stringp xs
+                                            return y::ys }
+                        | [] -> parser { return [] }
