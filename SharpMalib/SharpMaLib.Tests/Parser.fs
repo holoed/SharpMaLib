0d3a042d50dfbe00d0e59c880bae94a9109b37b5
holoed_cp <holorf>
2010-02-14 19:16:19 +0000

    Refactored Parser to works on strings instead of lists.

--- SharpMalib/SharpMaLib.Tests/Parser.fs
+++ SharpMalib/SharpMaLib.Tests/Parser.fs
@@ -28,12 +28,12 @@ module ParserTests =
         
         [<Test>]
         member this.Item() =
-            Assert.AreEqual ([('H', ['e';'l';'l';'o'])], parse item "Hello")
+            Assert.AreEqual ([('H', "ello")], parse item "Hello")
             Assert.AreEqual ([], parse item "")
 
         [<Test>]
         member this.Ret() = 
-            Assert.AreEqual ([('X', ['4';'2'])], parse (ret 'X') "42")
+            Assert.AreEqual ([('X', "42")], parse (ret 'X') "42")
 
         [<Test>]
         member this.Fail() =
@@ -44,8 +44,8 @@ module ParserTests =
             let parserA = ret 'X'
             let parserB = fail
             let input = "42"
-            Assert.AreEqual (['X', ['4';'2']], parse (parserA +++ parserB) input)
-            Assert.AreEqual (['X', ['4';'2']], parse (parserB +++ parserA) input)
+            Assert.AreEqual (['X', "42"], parse (parserA +++ parserB) input)
+            Assert.AreEqual (['X', "42"], parse (parserB +++ parserA) input)
 
         [<Test>]
         member this.Bind() = 
@@ -53,7 +53,7 @@ module ParserTests =
                          ret ['Y'] >>=  fun s2 -> 
                          ret ([s1] @ [s2])
             let ret = parse parser "42"
-            Assert.AreEqual([([['X'];['Y']], ['4';'2'])], ret)            
+            Assert.AreEqual([([['X'];['Y']], "42")], ret)            
 
         [<Test>]
         member this.ReturnFrom() =
@@ -62,7 +62,7 @@ module ParserTests =
                                         return! (f (x - 1) (List.tail y) ) }
             let ret = f 2 ([sat '4'; sat '2'])
             let actual = parse ret "42"
-            Assert.AreEqual( [('4', ['2'])], actual)
+            Assert.AreEqual( [('4', "2")], actual)
 
         [<Test>]
         member this.Zero() =
@@ -72,23 +72,23 @@ module ParserTests =
 
         [<Test>]
         member this.Combine() =
-            Assert.AreEqual([('H', ['e';'l';'l';'o'])], parse (parser { let! y = item 
-                                                                        if true then ()
-                                                                        return y}) "Hello")
+            Assert.AreEqual([('H', "ello")], parse (parser { let! y = item 
+                                                             if true then ()
+                                                             return y}) "Hello")
 
         [<Test>]
         member this.Sat() =
             Assert.AreEqual ([], parse (sat 'X') "Hello")
-            Assert.AreEqual ([('H', ['e';'l';'l';'o'])], parse (sat 'H') "Hello")
+            Assert.AreEqual ([('H', "ello")], parse (sat 'H') "Hello")
 
         [<Test>]
         member this.StringP() =
             Assert.AreEqual([], parse (stringp (Seq.toList "Hello")) "World")
-            Assert.AreEqual([(['H';'e';'l';'l';'o'], [' ';'W';'o';'r';'l';'d'])], parse (stringp (Seq.toList "Hello")) "Hello World")
+            Assert.AreEqual([(['H';'e';'l';'l';'o'], " World")], parse (stringp (Seq.toList "Hello")) "Hello World")
 
         [<Test>]
         member this.NonDeterministicChoice() =
-            Assert.AreEqual ([('X', ['H';'e';'l';'l';'o']); ('Y', ['H';'e';'l';'l';'o'])], parse (parser { return 'X' ; return 'Y' }) "Hello")
+            Assert.AreEqual ([('X', "Hello"); ('Y', "Hello")], parse (parser { return 'X' ; return 'Y' }) "Hello")
 
         [<Test>]
         [<Ignore("Still Working on this one")>]
---
+++ SharpMalib/SharpMalib/StringUtils.fs
@@ -0,0 +1,3 @@
+﻿module StringUtils
+
+let (|Empty|Cons|) (xs:string) : Choice<Unit, char * string> = if (Seq.isEmpty xs) then Empty else Cons(Seq.head xs, xs.Substring 1)
