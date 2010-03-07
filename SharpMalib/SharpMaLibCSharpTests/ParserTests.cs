e7738e47a7a9b105854042fdaecd53bda1ef2fb3
holoed_cp <holorf>
2010-03-07 00:53:58 +0000

    Added C# support for Parser Monad

---
+++ SharpMalib/SharpMaLibCSharpTests/ParserTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FsCheck;
+using NUnit.Framework;
+using SharpMaLib.Tests;
+using SharpMalib.Parser;
+
+namespace SharpMaLibCSharpTests
+{
+    [TestFixture]
+    public class ParserTests
+    {
+        private Configuration _configuration;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _configuration = new Configuration { Runner = NUnitFsCheck.NUnitRunner };
+        }
+
+        [Test]
+        public void Select()
+        {
+            var p = from x in ParserMonad.item<char>()
+                    select String.Format("({0})",x);
+            CollectionAssert.AreEqual(new[]{"(H)"}, p.Parse("Hello"));          
+        }
+
+        [Test]
+        public void SelectMany()
+        {
+            var p = from x in ParserMonad.item<char>()
+                    from y in ParserMonad.item<char>()
+                    select String.Format("({0}{1})", x, y);
+            CollectionAssert.AreEqual(new[] { "(He)" }, p.Parse("Hello"));
+        }
+
+        [Test]
+        public void Add()
+        {
+            var q = from x in "2 + 3".Eval()
+                    from y in "3 + 2".Eval()
+                    select x == y;
+            Assert.IsTrue(q.First());
+        }
+
+        [Test]
+        public void ArithmeticProperties()
+        {
+            Spec.ForAny<int>(x => CollectionAssert.AreEqual(Calculate("0 + {0}", x), Calculate("{0} + 0", x))).Check(_configuration);
+            Spec.ForAny<int>(x => CollectionAssert.AreEqual(Calculate("1 * {0}", x), Calculate("{0} * 1", x))).Check(_configuration);
+
+            Spec.ForAny<int, int>((x, y) => CollectionAssert.AreEqual(Calculate("{1} + {0}", x, y), Calculate("{0} + {1}", x, y))).Check(_configuration);
+            Spec.ForAny<int, int>((x, y) => CollectionAssert.AreEqual(Calculate("{1} * {0}", x, y), Calculate("{0} * {1}", x, y))).Check(_configuration);
+        }    
+
+        private static IEnumerable<int> Calculate(string format, params object[] args)
+        {
+            return string.Format(format, args).Eval().ToArray();
+        }
+    }
+}
