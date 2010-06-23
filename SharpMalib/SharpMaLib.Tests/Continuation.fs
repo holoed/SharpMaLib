bf692651c2cb897f9a0b7e468beb1390e0a26e2d
holoed_cp <holorf>
2010-06-23 22:16:30 +0000

    Added simple Continuation Monad implementation

---
+++ SharpMalib/SharpMaLib.Tests/Continuation.fs
@@ -0,0 +1,44 @@
+﻿
+
+// * **********************************************************************************************
+// * Copyright (c) Edmondo Pentangelo. 
+// *
+// * This source code is subject to terms and conditions of the Microsoft Public License. 
+// * A copy of the license can be found in the License.html file at the root of this distribution. 
+// * By using this source code in any fashion, you are agreeing to be bound by the terms of the 
+// * Microsoft Public License.
+// *
+// * You must not remove this notice, or any other, from this software.
+// * **********************************************************************************************
+
+module ContinuationTests
+
+open NUnit.Framework
+open NUnitFsCheck
+open Monad.Continuation
+open System
+
+
+[<TestFixture>]
+type ContinuationTests =
+    new() = {}
+    
+    [<Test>]
+    member x.ExampleOfUse() =
+        
+       let facCPS n =
+                let rec loop n =
+                  cont {
+                          match n with
+                          | n when n <= 0 -> return 1
+                          | _ -> let! x = fun f -> f n
+                                 let! y = loop (n - 1)
+                                 return x * y
+                       }
+                loop n (fun x -> x)
+
+       let product = List.fold (*) 1
+
+       quickCheck (fun n -> facCPS n = product [1..n])
+        
+
---
+++ SharpMalib/SharpMalib/Continuation.fs
@@ -0,0 +1,38 @@
+﻿// * **********************************************************************************************
+// * Copyright (c) Edmondo Pentangelo. 
+// *
+// * This source code is subject to terms and conditions of the Microsoft Public License. 
+// * A copy of the license can be found in the License.html file at the root of this distribution. 
+// * By using this source code in any fashion, you are agreeing to be bound by the terms of the 
+// * Microsoft Public License.
+// *
+// * You must not remove this notice, or any other, from this software.
+// * **********************************************************************************************
+
+namespace Monad
+module Continuation = 
+
+    open Combinators
+    open Utils
+
+    // Continuation Monad
+    // Continuations represent the future of a computation, as a function from an intermediate result to the final result. 
+    // In continuation-passing style, computations are built up from sequences of nested continuations, 
+    // terminated by a final continuation (often id) which produces the final result. Since continuations are functions which represent the future of a computation, 
+    // manipulation of the continuation functions can achieve complex manipulations of the future of the computation, such as interrupting a computation in the middle, 
+    // aborting a portion of a computation, restarting a computation and interleaving execution of computations. 
+    // The Continuation monad adapts CPS to the structure of a monad.
+
+    type ContinuationMonad() =
+        // ma -> (a -> mb) -> mb
+        member this.Bind (m, f) = fun c -> m (fun a -> f a c)
+        // a -> ma
+        member this.Return x = fun k -> k x
+        // ma -> ma
+        member this.ReturnFrom m = m
+
+    let cont = ContinuationMonad()
+
+   
+    
+
